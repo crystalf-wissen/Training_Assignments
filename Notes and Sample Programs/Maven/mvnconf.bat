16296abcbf82e70a65e875f8509d20ccb3695708
Crystal Fernandes <crystal.fernandes@wissen.com>
2025-02-11 16:52:48 +0530

    maven

---
+++ Notes and Sample Programs/Maven/mvnconf.bat
@@ -0,0 +1,4 @@
+set M2_HOME="C:\Users\Wissen\Desktop\Assignment\Notes and Sample Programs\Maven\apache-maven-3.9.9"
+set MAVEN_HOME="C:\Users\Wissen\Desktop\Assignment\Notes and Sample Programs\Maven\apache-maven-3.9.9"
+set path=%MAVEN_HOME%\bin;.;%path%
+start
