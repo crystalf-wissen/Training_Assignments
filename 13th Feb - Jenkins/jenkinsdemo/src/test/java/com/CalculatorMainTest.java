310fadbf7b12017fecde42061a71480c0033b2b9
Crystal Fernandes <crystal.fernandes@wissen.com>
2025-02-13 15:58:49 +0530

    jenkins

---
+++ 13th Feb - Jenkins/jenkinsdemo/src/test/java/com/CalculatorMainTest.java
@@ -0,0 +1,15 @@
+package com;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+public class CalculatorMainTest{
+    @Test
+    public void testAdd(){
+        Calculator c2 = new Calculator();
+        assertEquals(4,c2.add(2,2),"The sum of 2 and 2 is not resulting in 4");
+        assertEquals(8,c2.add(4,4),"The sum of 4 and 4 is not resulting in 8");
+    }
+
+
+}
