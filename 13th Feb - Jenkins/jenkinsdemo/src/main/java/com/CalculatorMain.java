89ce179cb4d2294728a5a6320933620b9ce152c8
Crystal Fernandes <crystal.fernandes@wissen.com>
2025-02-13 16:40:48 +0530

    jenkins

--- 13th Feb - Jenkins/jenkinsdemo/src/main/java/com/CalculatorMain.java
+++ 13th Feb - Jenkins/jenkinsdemo/src/main/java/com/CalculatorMain.java
@@ -6,9 +6,13 @@ class Calculator
 		System.out.println("Add : "+(a+b));
 		return a+b;
 	}
-	public void sub(int a, int b)
-	{
+	public int sub(int a, int b) {
 		System.out.println("Subtract : "+(a-b));
+		return a-b;
+	}
+	public int mul(int a, int b) {
+		System.out.println("Multiply : "+(a-b));
+		return a*b;
 	}
 }
 public class CalculatorMain
@@ -18,7 +22,8 @@ public class CalculatorMain
 		System.out.println("Calculator Operations......");
  
 		Calculator c1 = new Calculator();
-		int a = c1.add(10, 20);
+		c1.add(10, 20);
 		c1.sub(10, 20);
+		c1.mul(10, 20);
 	}
 }
